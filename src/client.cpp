8e1e41295616e7e20d63c693e203be4741605e27
Mohammed reda Oulmadou <moulmado@e1r12p5.1337.ma>
2023-06-09 15:50:45 +0100

    recieving chunked requests

--- src/client.cpp
+++ src/client.cpp
@@ -58,36 +58,36 @@ void Client::addChunkedBody()
 {
 	std::string rawBody;
 
-	while (rawBody.size())
-	{
+	std::cout << "rawContent: " << rawContent << std::endl;
+	while (rawContent.size()) {
 		rawBody = rawContent.substr(0, rawContent.find('\n'));
-		rawContent.erase(rawContent.begin(), rawContent.begin() + rawBody.size());
-		if (chunkSize == -1)
-		{
-			try
-			{
+		std::cout << "--rawBody: " << rawBody << "|--" << std::endl;
+		rawContent.erase(rawContent.begin(), rawContent.begin() + rawBody.size() + 1);
+		if (chunkSize == -1) {
+			try {
 				chunkSize = std::stoi(rawBody);
+				if (chunkSize < 0) {
+					request.setValidity(false);
+					return;
+				}
+				else if (chunkSize == 0) {
+					rawContent.erase(rawContent.begin(), rawContent.begin() + 2);
+					return;
+				}
 			}
-			catch (const std::exception &e)
-			{
+			catch (const std::exception &e) {
 				request.setValidity(false);
 			}
-		}
-		else if (chunkSize < 0)
-			request.setValidity(false);
-		else if (chunkSize > 0)
-		{
+		} 
+		else if (chunkSize > 0) {
 			rawBody = rawContent.substr(0, rawContent.find('\n'));
 			rawContent.erase(rawContent.begin(), rawContent.begin() + rawBody.size());
-			if ((int)rawBody.size() != chunkSize)
-			{
+			if ((int)rawBody.size() != chunkSize) {
 				request.setValidity(false);
 				return;
 			}
 			forup(i, 0, rawContent.size()) request.getBody().push_back(rawBody[i]);
 		}
-		else
-			return;
 	}
 }
 
@@ -104,39 +104,42 @@ void Client::addRawRequest(char *buffer, size_t size)
 {
 	forup(i, 0, size) this->rawContent.push_back(buffer[i]);
 	// std::cout << "rawContent: \n" << rawContent << std::endl;
-	if (request.empty() && rawContent.find("\r\n\r\n") != std::string::npos)
-	{
+	if (request.empty() && rawContent.find("\r\n\r\n") != std::string::npos) {
 		splitRawRequest();
 		request.addRawContent(rawContent);
 		forup(i, 0, request.getRequestContent().size())	std::cout << request.getRequestContent()[i] << std::endl;
 		request.parse();
 		this->rawContent.erase(this->rawContent.begin(), this->rawContent.begin() + this->rawContent.find("\r\n\r\n") + 4);
-		if (!request.isValid() || !request.bodyDoesExist())
-		{
+		if (!request.isValid() || !request.bodyDoesExist()) {
 			response.generateResponse(request, srvconf);
 			nextRequest();
 			return;
 		}
-		else
-		{
+		else {
 			rawContent = remaining;
 			remaining.clear();
 		}
 	}
-	if (request.isRequestChunked())
-	{
+	if (request.isRequestChunked()) {
+		std::cout << "1" << std::endl;
 		addChunkedBody();
-		if (chunkSize == 0)
-		{
+		std::cout << "2" << std::endl;
+		if (!request.isValid()) {
+			std::cout << "3" << std::endl;
 			response.generateResponse(request, srvconf);
+			chunkSize = -1;
+			return;
+		}
+		if (chunkSize == 0) {
+			std::cout << "4" << std::endl;
+			response.generateResponse(request, srvconf);
+			chunkSize = -1;
 			return;
 		}
 	}
-	if (request.bodyDoesExist())
-	{
+	else if (request.bodyDoesExist()) {
 		addNormalBody();
-		if ((int)request.getBody().size() == request.getBodyLength())
-		{
+		if ((int)request.getBody().size() == request.getBodyLength()) {
 			forup(i, 0, request.getBody().size()) std::cout << request.getBody()[i];
 			std::cout << std::endl;
 			response.generateResponse(request, srvconf);
